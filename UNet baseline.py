0dbd6e483b8985fe96582d6218638d988e1f4f41
Timur Reziapov <timur.reziapov@gmail.com>
2019-11-07 22:56:54 -0800

    initial commit for UNet baseline
    includes input data processing for training/testing/submission

---
+++ UNet baseline.py
@@ -0,0 +1,615 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# !pip install tensorflow
+# !pip install keras pydot opencv-python tqdm sklearn segmentation-models\
+#     albumentations
+
+
+# In[3]:
+
+
+# !pip uninstall tensorflow-gpu
+
+
+# In[4]:
+
+
+# !pip install --upgrade tensorflow-gpu==1.8.0
+
+
+# # Import libraries
+
+# In[1]:
+
+
+import os
+import json
+
+import cv2
+import keras
+from keras import backend as K
+from keras.models import Model
+from keras.layers import Input
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.losses import binary_crossentropy
+from keras.callbacks import Callback, ModelCheckpoint
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+import segmentation_models as sm
+import albumentations as albu
+
+
+# In[6]:
+
+
+# Check GPU availability
+from tensorflow.python.client import device_lib
+
+print(device_lib.list_local_devices())
+
+
+# # Load and prepare the training data
+
+# In[7]:
+
+
+train_df = pd.read_csv('./train.csv')
+train_df['ImageId'] = train_df['Image_Label'].apply(lambda x: x.split('_')[0])
+train_df['ClassId'] = train_df['Image_Label'].apply(lambda x: x.split('_')[1])
+train_df['hasMask'] = ~ train_df['EncodedPixels'].isna()
+
+print(train_df.shape)
+train_df.head()
+
+
+# In[8]:
+
+
+mask_count_df = train_df.groupby('ImageId').agg(np.sum).reset_index()
+mask_count_df.sort_values('hasMask', ascending=False, inplace=True)
+print(mask_count_df.shape)
+mask_count_df.head()
+
+
+# In[9]:
+
+
+sub_df = pd.read_csv('./sample_submission.csv')
+sub_df['ImageId'] = sub_df['Image_Label'].apply(lambda x: x.split('_')[0])
+test_imgs = pd.DataFrame(sub_df['ImageId'].unique(), columns=['ImageId'])
+
+
+# # Helper functions
+
+# In[10]:
+
+
+def np_resize(img, input_shape):
+    """
+    Reshape a numpy array, which is input_shape=(height, width), 
+    as opposed to input_shape=(width, height) for cv2
+    """
+    height, width = input_shape
+    return cv2.resize(img, (width, height))
+    
+def mask2rle(img):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels= img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+def rle2mask(rle, input_shape):
+    width, height = input_shape[:2]
+    
+    mask= np.zeros( width*height ).astype(np.uint8)
+    
+    array = np.asarray([int(x) for x in rle.split()])
+    starts = array[0::2]
+    lengths = array[1::2]
+
+    current_position = 0
+    for index, start in enumerate(starts):
+        mask[int(start):int(start+lengths[index])] = 1
+        current_position += lengths[index]
+        
+    return mask.reshape(height, width).T
+
+def build_masks(rles, input_shape, reshape=None):
+    depth = len(rles)
+    if reshape is None:
+        masks = np.zeros((*input_shape, depth))
+    else:
+        masks = np.zeros((*reshape, depth))
+    
+    for i, rle in enumerate(rles):
+        if type(rle) is str:
+            if reshape is None:
+                masks[:, :, i] = rle2mask(rle, input_shape)
+            else:
+                mask = rle2mask(rle, input_shape)
+                reshaped_mask = np_resize(mask, reshape)
+                masks[:, :, i] = reshaped_mask
+    
+    return masks
+
+def build_rles(masks, reshape=None):
+    width, height, depth = masks.shape
+    
+    rles = []
+    
+    for i in range(depth):
+        mask = masks[:, :, i]
+        
+        if reshape:
+            mask = mask.astype(np.float32)
+            mask = np_resize(mask, reshape).astype(np.int64)
+        
+        rle = mask2rle(mask)
+        rles.append(rle)
+        
+    return rles
+
+
+# # Loss function
+
+# In[11]:
+
+
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+
+# # Sample Test
+
+# In[12]:
+
+
+sample_filename = '002be4f.jpg'
+sample_image_df = train_df[train_df['ImageId'] == sample_filename]
+sample_path = f"./train_images/{sample_image_df['ImageId'].iloc[0]}"
+sample_img = cv2.imread(sample_path)
+sample_rles = sample_image_df['EncodedPixels'].values
+sample_masks = build_masks(sample_rles, input_shape=(1400, 2100))
+
+fig, axs = plt.subplots(5, figsize=(12, 12))
+axs[0].imshow(sample_img)
+axs[0].axis('off')
+
+for i in range(4):
+    axs[i+1].imshow(sample_masks[:, :, i])
+    axs[i+1].axis('off')
+
+
+# # Data generator
+
+# In[69]:
+
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, df, target_df=None, mode='fit',
+                 base_path='/Users/treziapov/Documents/JupyterNotebooks/cs230/train_images',
+                 batch_size=32, dim=(1400, 2100), n_channels=3, reshape=None,
+                 augment=False, n_classes=4, random_state=2019, shuffle=True):
+        self.dim = dim
+        self.batch_size = batch_size
+        self.df = df
+        self.mode = mode
+        self.base_path = base_path
+        self.target_df = target_df
+        self.list_IDs = list_IDs
+        self.reshape = reshape
+        self.n_channels = n_channels
+        self.augment = augment
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.random_state = random_state
+        
+        self.on_epoch_end()
+        np.random.seed(self.random_state)
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_batch = [self.list_IDs[k] for k in indexes]
+        
+        X = self.__generate_X(list_IDs_batch)
+        
+        if self.mode == 'fit':
+            y = self.__generate_y(list_IDs_batch)
+            
+            if self.augment:
+                X, y = self.__augment_batch(X, y)
+            
+            return X, y
+        
+        elif self.mode == 'predict':
+            return X
+
+        else:
+            raise AttributeError('The mode parameter should be set to "fit" or "predict".')
+        
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.seed(self.random_state)
+            np.random.shuffle(self.indexes)
+    
+    def __generate_X(self, list_IDs_batch):
+        'Generates data containing batch_size samples'
+        # Initialization
+        if self.reshape is None:
+            X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        else:
+            X = np.empty((self.batch_size, *self.reshape, self.n_channels))
+        
+        # Generate data
+        for i, ID in enumerate(list_IDs_batch):
+            im_name = self.df['ImageId'].iloc[ID]
+            img_path = f"{self.base_path}/{im_name}"
+            img = self.__load_rgb(img_path)
+            
+            if self.reshape is not None:
+                img = np_resize(img, self.reshape)
+            
+            # Store samples
+            X[i,] = img
+
+        return X
+    
+    def __generate_y(self, list_IDs_batch):
+        if self.reshape is None:
+            y = np.empty((self.batch_size, *self.dim, self.n_classes), dtype=int)
+        else:
+            y = np.empty((self.batch_size, *self.reshape, self.n_classes), dtype=int)
+        
+        for i, ID in enumerate(list_IDs_batch):
+            im_name = self.df['ImageId'].iloc[ID]
+            image_df = self.target_df[self.target_df['ImageId'] == im_name]
+            
+            rles = image_df['EncodedPixels'].values
+            
+            if self.reshape is not None:
+                masks = build_masks(rles, input_shape=self.dim, reshape=self.reshape)
+            else:
+                masks = build_masks(rles, input_shape=self.dim)
+            
+            y[i, ] = masks
+
+        return y
+    
+    def __load_grayscale(self, img_path):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = img.astype(np.float32) / 255.
+        img = np.expand_dims(img, axis=-1)
+
+        return img
+    
+    def __load_rgb(self, img_path):
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.astype(np.float32) / 255.
+
+        return img
+    
+    def __random_transform(self, img, masks):
+        composition = albu.Compose([
+            albu.HorizontalFlip(),
+            albu.VerticalFlip(),
+            albu.ShiftScaleRotate(rotate_limit=45, shift_limit=0.15, scale_limit=0.15)
+        ])
+        
+        composed = composition(image=img, mask=masks)
+        aug_img = composed['image']
+        aug_masks = composed['mask']
+        
+        return aug_img, aug_masks
+    
+    def __augment_batch(self, img_batch, masks_batch):
+        for i in range(img_batch.shape[0]):
+            img_batch[i, ], masks_batch[i, ] = self.__random_transform(
+                img_batch[i, ], masks_batch[i, ])
+        
+        return img_batch, masks_batch
+
+
+# # Vanilla UNet
+
+# In[14]:
+
+
+def vanilla_unet(input_shape):
+    """
+    This is the old model. Best LB is ~0.5
+    """
+    inputs = Input(input_shape)
+
+    c1 = Conv2D(8, (3, 3), activation='elu', padding='same') (inputs)
+    c1 = Conv2D(8, (3, 3), activation='elu', padding='same') (c1)
+    p1 = MaxPooling2D((2, 2), padding='same') (c1)
+
+    c2 = Conv2D(16, (3, 3), activation='elu', padding='same') (p1)
+    c2 = Conv2D(16, (3, 3), activation='elu', padding='same') (c2)
+    p2 = MaxPooling2D((2, 2), padding='same') (c2)
+
+    c3 = Conv2D(32, (3, 3), activation='elu', padding='same') (p2)
+    c3 = Conv2D(32, (3, 3), activation='elu', padding='same') (c3)
+    p3 = MaxPooling2D((2, 2), padding='same') (c3)
+
+    c4 = Conv2D(64, (3, 3), activation='elu', padding='same') (p3)
+    c4 = Conv2D(64, (3, 3), activation='elu', padding='same') (c4)
+    p4 = MaxPooling2D((2, 2), padding='same') (c4)
+
+    c5 = Conv2D(64, (3, 3), activation='elu', padding='same') (p4)
+    c5 = Conv2D(64, (3, 3), activation='elu', padding='same') (c5)
+    p5 = MaxPooling2D((2, 2), padding='same') (c5)
+
+    c55 = Conv2D(128, (3, 3), activation='elu', padding='same') (p5)
+    c55 = Conv2D(128, (3, 3), activation='elu', padding='same') (c55)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c55)
+    u6 = concatenate([u6, c5])
+    c6 = Conv2D(64, (3, 3), activation='elu', padding='same') (u6)
+    c6 = Conv2D(64, (3, 3), activation='elu', padding='same') (c6)
+
+    u71 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
+    u71 = concatenate([u71, c4])
+    c71 = Conv2D(32, (3, 3), activation='elu', padding='same') (u71)
+    c61 = Conv2D(32, (3, 3), activation='elu', padding='same') (c71)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c61)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='elu', padding='same') (u7)
+    c7 = Conv2D(32, (3, 3), activation='elu', padding='same') (c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='elu', padding='same') (u8)
+    c8 = Conv2D(16, (3, 3), activation='elu', padding='same') (c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='elu', padding='same') (u9)
+    c9 = Conv2D(8, (3, 3), activation='elu', padding='same') (c9)
+
+    outputs = Conv2D(4, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    return model
+
+
+# # Training
+
+# In[15]:
+
+
+BATCH_SIZE = 16
+
+train_idx, val_idx = train_test_split(
+    mask_count_df.index, random_state=2019, test_size=0.2
+)
+
+train_generator = DataGenerator(
+    train_idx, 
+    df=mask_count_df,
+    target_df=train_df,
+    batch_size=BATCH_SIZE,
+    reshape=(320, 480),
+    augment=False,
+    n_channels=3,
+    n_classes=4
+)
+
+val_generator = DataGenerator(
+    val_idx, 
+    df=mask_count_df,
+    target_df=train_df,
+    batch_size=BATCH_SIZE, 
+    reshape=(320, 480),
+    augment=False,
+    n_channels=3,
+    n_classes=4
+)
+
+
+# # Define model and display summary
+
+# In[17]:
+
+
+model = vanilla_unet((320, 480, 3))
+model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[dice_coef])
+model.summary()
+
+
+# ## Training history
+
+# In[26]:
+
+
+checkpoint = ModelCheckpoint(
+    'weights_from10.{epoch:02d}-{val_loss:.2f}.hdf5', 
+    monitor='val_dice_coef', 
+    verbose=1, 
+    save_best_only=False, 
+    save_weights_only=False,
+    mode='max'
+)
+
+history = model.fit_generator(
+    train_generator,
+    validation_data=val_generator,
+    callbacks=[checkpoint],
+    use_multiprocessing=False,
+    workers=1,
+    epochs=1
+)
+
+
+# In[70]:
+
+
+model.load_weights('weights.10-0.76.hdf5')
+model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[dice_coef])
+model.summary()
+
+checkpoint = ModelCheckpoint(
+    'weights_from10.{epoch:02d}-{val_loss:.2f}.hdf5', 
+    monitor='val_dice_coef', 
+    verbose=1, 
+    save_best_only=False, 
+    save_weights_only=False,
+    mode='max'
+)
+
+history = model.fit_generator(
+    train_generator,
+    validation_data=val_generator,
+    callbacks=[checkpoint],
+    use_multiprocessing=False,
+    workers=1,
+    epochs=90
+)
+
+
+# # Evaluation
+
+# In[ ]:
+
+
+with open('history.json', 'w') as f:
+    json.dump(str(history.history), f)
+    
+history_df = pd.DataFrame(history.history)
+
+
+# In[68]:
+
+
+history_df[['loss', 'val_loss']].plot()
+plt.title('Loss')
+plt.legend(['train','test'], loc='right')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.savefig("unet_baseline_10epoch_loss.png")
+
+history_df[['dice_coef', 'val_dice_coef']].plot()
+plt.title('Dice coefficient')
+plt.legend(['train','test'], loc='right')
+plt.ylabel('Dice coefficient')
+plt.xlabel('epoch')
+plt.savefig("unet_baseline_10epoch_dice.png")
+
+
+# In[ ]:
+
+
+# load history from JSON
+with open('history.json', 'r') as f:
+    history = json.load(f)
+
+history_df = pd.DataFrame(history)
+
+
+# # Submission
+
+# In[55]:
+
+
+model.load_weights('weights.10-0.76.hdf5')
+test_df = []
+
+for i in range(0, test_imgs.shape[0], 500):
+    batch_idx = list(
+        range(i, min(test_imgs.shape[0], i + 500))
+    )
+
+    test_generator = DataGenerator(
+        batch_idx,
+        df=test_imgs,
+        shuffle=False,
+        mode='predict',
+        dim=(350, 525),
+        reshape=(320, 480),
+        n_channels=3,
+        base_path='./test_images',
+        target_df=sub_df,
+        batch_size=1,
+        n_classes=4
+    )
+
+    batch_pred_masks = model.predict_generator(
+        test_generator, 
+        workers=1,
+        verbose=1
+    )
+
+    for j, b in enumerate(batch_idx):
+        filename = test_imgs['ImageId'].iloc[b]
+        image_df = sub_df[sub_df['ImageId'] == filename].copy()
+
+        pred_masks = batch_pred_masks[j, ].round().astype(int)
+        pred_rles = build_rles(pred_masks, reshape=(350, 525))
+
+        image_df['EncodedPixels'] = pred_rles
+        test_df.append(image_df)
+
+
+# In[61]:
+
+
+test_df
+
+
+# In[62]:
+
+
+# test_df = pd.concat(test_df)
+# test_df.drop(columns='ImageId', inplace=True)
+test_df.to_csv('submission.csv', index=False)
+
+
+# In[ ]:
+
+
+
+
